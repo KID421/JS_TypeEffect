35329693253562ca0ea91d232fe3e39c2db00913
KID421 <suchild7@gmail.com>
2019-09-26 19:03:56 +0800

    設定重複時間

--- typeEffect.js
+++ typeEffect.js
@@ -1,5 +1,5 @@
 var texts = ["網頁設計師", "插畫家", "遊戲開發者"];   // 陣列
-var speed = 1000;                                   // 打字速度
+var speed = 600;                                   // 打字速度
 var index = 0;                                      // 編號
 var count = 0;                                      // 字數
 var id = "type-box";                                // 要寫入文字的 ID
@@ -9,10 +9,13 @@ function type(){
     var box = document.getElementById(id);
 
     // 變數 切割完的文字 = 文字陣列[編號].切割(開始位置，切幾個);
-    var slice = texts[index].slice(0, 3);
+    var slice = texts[index].slice(0, count++);
 
     // 盒子.插入內容 = 切割完的文字;
     box.innerHTML = slice;
+
+    // 設定重複時間(方法名稱，間隔時間 (毫秒))
+    setTimeout(type, speed);
 }
 
 // 當網頁載入完成時執行方法 type
